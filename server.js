f41934ed51b782934a1547329e0d3454df69badb
tejaswini709 <tejaswinins.0302@gmail.com>
2025-10-05 18:32:58 +0530

    Add server.js and routes for HTML pages

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const PORT = 3000;
 // Serve all static files (HTML, CSS, JS, images)
 app.use(express.static(__dirname));
 
-// ✅ Default route – send home.html when user opens "/"
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
+// Routes for your HTML pages
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+app.get("/product", (req, res) => res.sendFile(path.join(__dirname, "product.html")));
+app.get("/cart", (req, res) => res.sendFile(path.join(__dirname, "cart.html")));
+app.get("/billing", (req, res) => res.sendFile(path.join(__dirname, "billing.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
+app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
+app.get("/adminlogin", (req, res) => res.sendFile(path.join(__dirname, "adminlogin.html")));
 
 // Start server
 app.listen(PORT, () => {
